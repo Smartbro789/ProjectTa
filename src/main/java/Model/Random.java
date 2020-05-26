6c62816ad5b2b45e6f81f11c49e2273d0806ee5f
BuildTools <unconfigured@null.spigotmc.org>
2020-05-26 12:55:20 +0300

    Ya yebal github

--- src/main/java/Model/Random.java
+++ src/main/java/Model/Random.java
@@ -4,5 +4,6 @@ public class Random {
     public static void main(String[] args) {
         System.out.println("pidaras");
         System.out.println("PIZDEC NAHUI BLYAT'");
+        System.out.println("Вас ждёт анальная расправа");
     }
 }
